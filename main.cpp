b59b9127837cf342ad3594b7e5a50d1e61989c31
Logan Saltiban <logansaltiban@Logans-MacBook-Air.local>
2024-09-27 13:32:45 -0700

    Third commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -36,7 +37,7 @@ int main()
 {
     // Create Input File Object & Open movies.txt
     ifstream movies;
-    movies.open("movies.txt")
+    movies.open("movies.txt");
 
     // Test Case Open File
     if (!movies)
@@ -45,9 +46,38 @@ int main()
         return 1;
     }
 
+    // Initialize Vector of Movies
+    vector<Movies> movieList;
+
+  
+        while (!movies.eof())
+        {
+            // Temp Variable Declarations
+            Movies tempMovie;
+            string tempScreenwriter = "";
+            int tempYearReleased = 0;
+            string tempMovieTitle = "";
+
+            // Set Movie Title
+            getline(movies, tempMovieTitle);
+            tempMovie.set_movieTitle(tempMovieTitle);
+            
+            // Set Year Released
+            movies >> tempYearReleased;
+            tempMovie.set_yearReleased(tempYearReleased);
+
+            // Set Screenwriter Name
+            movies.ignore(); // Needed since we use a >> operator beforehand
+            getline(movies, tempScreenwriter);
+            tempMovie.set_screenWriter(tempScreenwriter);
+
+            // Add The Filled TempMovie into movieList
+            movieList.push_back(tempMovie);
+        }
+
+        // Print Using Range
+        for (auto movie : movieList)
+            movie.printInfo();
     
-
-
-
     return 0;
 }
